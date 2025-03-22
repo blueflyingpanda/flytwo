e005d398101c5ae2ac1b0ae74336405c426231d0
blueflyingpanda <m.s.v.inkognito@gmail.com>
2025-03-22 13:48:54 +0200

    feat: API endpoints caching

--- src/api/api.py
+++ src/api/api.py
@@ -1,11 +1,29 @@
+from contextlib import asynccontextmanager
 from datetime import date
-
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
 
+from api.cache_utils import price_history_key_builder, DateJsonCoder
+from cache import redis_client
 from dal import DataAccessLayer
 from fly_client.client import FlyoneClient
+from logs import custom_logger
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        async with redis_client() as redis_cache:
+            FastAPICache.init(RedisBackend(redis_cache), prefix="api-cache")
+            yield
+    except Exception as e:
+        custom_logger.error(f"Error during lifespan setup: {e}")
+        raise
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get('/ping')
@@ -13,11 +31,18 @@ async def ping():
     return {'ping': 'pong'}
 
 @app.get('/price-history/{src}/{dst}')
+@cache(
+    expire=300,
+    key_builder=price_history_key_builder,
+    coder=DateJsonCoder,
+)  # Cache for 5 minutes
 async def price_history(src: str, dst: str, dt: date | None = None):
     return await DataAccessLayer.get_direction_price_history(src.upper(), dst.upper(), dt)
 
 @app.get('/airports')
-async def price_history():
+@cache(expire=3600)  # Cache for 1 hour
+async def airports():
     """Proxy endpoint to fetch airports from Flyone API."""
     fc = FlyoneClient()
+    custom_logger.info('Fetching airports')
     return await fc.airport_by_code
--- src/cache.py
+++ src/cache.py
@@ -1,12 +1,15 @@
 from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Any
 
 import redis.asyncio as redis
 from redis.asyncio import Redis
 
 from conf import REDIS_HOST, REDIS_PORT, REDIS_PASS, YC_CERT
+from logs import custom_logger
+
 
 @asynccontextmanager
-async def redis_client() -> Redis:
+async def redis_client() -> AsyncGenerator[Redis, Any]:
     cache = redis.Redis(
         host=REDIS_HOST,
         port=REDIS_PORT,
@@ -15,6 +18,10 @@ async def redis_client() -> Redis:
         ssl_ca_certs=YC_CERT,
     )
     try:
+        await cache.ping()
+        custom_logger.info('Redis connection successful')
         yield cache
+    except Exception as e:
+        custom_logger.error(e)
     finally:
         await cache.close()

994634a2d7e5c723b29f6afc609c1e11fb4eef4b
blueflyingpanda <m.s.v.inkognito@gmail.com>
2026-04-19 02:28:17 +0300

    ref: uvicorn better logging

--- src/logs.py
+++ src/logs.py
@@ -1,4 +1,14 @@
 import logging
 
+from uvicorn.logging import AccessFormatter
+
 logger = logging.getLogger('default')
 logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+handler.setFormatter(
+    AccessFormatter('%(asctime)s - %(name)s - %(levelname)s - %(client_addr)s - "%(request_line)s" %(status_code)s')
+)
+access_logger = logging.getLogger('uvicorn.access')
+access_logger.handlers = [handler]
+access_logger.propagate = False

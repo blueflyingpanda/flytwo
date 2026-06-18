0dbcdddb8285873754185a9bc29207b9d8182212
blueflyingpanda <m.s.v.inkognito@gmail.com>
2026-06-19 02:20:18 +0300

    fix: log auth extended setup

--- src/logs.py
+++ src/logs.py
@@ -4,11 +4,16 @@ from uvicorn.logging import AccessFormatter
 
 logger = logging.getLogger('default')
 logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
-handler = logging.StreamHandler()
-handler.setFormatter(
+_default_handler = logging.StreamHandler()
+_default_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(_default_handler)
+
+_access_handler = logging.StreamHandler()
+_access_handler.setFormatter(
     AccessFormatter('%(asctime)s - %(name)s - %(levelname)s - %(client_addr)s - "%(request_line)s" %(status_code)s')
 )
 access_logger = logging.getLogger('uvicorn.access')
-access_logger.handlers = [handler]
+access_logger.addHandler(_access_handler)
 access_logger.propagate = False

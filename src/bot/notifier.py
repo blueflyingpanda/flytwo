ebcadbc48da721c6b6234a06c2310a14224fc243
blueflyingpanda <m.s.v.inkognito@gmail.com>
2026-06-28 17:34:05 +0300

    ref: more space in links

--- src/bot/notifier.py
+++ src/bot/notifier.py
@@ -141,7 +141,7 @@ class TgBotNotifier:
 
             for msg in msgs:
                 payload = {
-                    'text': f'<pre>{self.msg_header}\n\n{msg}</pre>\n\n{"\n".join(self.links.values())}'.strip(),
+                    'text': f'<pre>{self.msg_header}\n\n{msg}</pre>\n\n{"\n\n".join(self.links.values())}'.strip(),
                     'chat_id': self.chat_id,
                     'parse_mode': 'HTML',
                 }
